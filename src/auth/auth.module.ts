d5952edcd88d47fc07af8e13f9f121c0f54df938
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2025-03-06 12:30:39 -0300

    feat: insert Swagger UI

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,31 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 import { GlobalExceptionsFilter } from './exceptions/global-exceptions.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Financial Management API')
+    .setDescription('API for a personal financial management system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
 
   const configService = app.get(ConfigService);
   const port = process.env.PORT ?? 3000;
@@ -19,6 +39,7 @@ async function bootstrap() {
     new HttpExceptionFilter(configService),
     new GlobalExceptionsFilter(httpAdapterHost),
   );
+
   await app.listen(port);
 }
 
