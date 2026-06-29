c8fd7ee2debc2b574f0c9e10878853ee0d797218
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2026-06-29 16:20:23 -0300

    enable cors for multiple domains

--- src/main.ts
+++ src/main.ts
@@ -31,8 +31,9 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   app.enableCors({
-    origin:
-      configService.get<string>('FRONTEND_URL') ?? 'http://localhost:5173',
+    origin: configService.get<string>('FRONTEND_URLS')?.split(',') ?? [
+      'http://localhost:5173',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   });
